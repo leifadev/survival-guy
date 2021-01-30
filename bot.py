d0600dfc545ddd51f5abe0f3b5fb3a63ea7baed2
vanillahow <49649192+leifadev@users.noreply.github.com>
2021-01-30 15:12:47 -0600

    quick sync update

--- bot.py
+++ bot.py
@@ -3,12 +3,13 @@ import discord
 import discord.ext
 from discord.ext import commands
 from discord.utils import get
-
+import time
 
 intents = discord.Intents.all()
 initial_extensions = ['cogs.cogs']
 token = BOT['TOKEN']
 bot = commands.Bot(command_prefix=BOT['PREFIX'], intents=intents, help_command=None)
+
 
 class MyHelpCommand(commands.MinimalHelpCommand):
     def get_command_signature(self, command):
@@ -54,27 +55,89 @@ async def on_raw_reaction_add(payload):
                 emoji = payload.emoji.name
                 await message.remove_reaction(emoji, user)
 
+
 @bot.command()
 async def kawaii(ctx):
     await ctx.channel.purge(limit=1)
     await ctx.channel.send(file=discord.File('pics/02.gif'))
-                
+
+
 @bot.command()
 async def help(ctx):
     rembed = discord.Embed(title="IP and Info", color=0x3a88fe)
-    rembed.add_field(name="The IP to the server is yugen.us.to", value="Contact admins for any other concerns", inline=False)
+    rembed.add_field(name="The IP to the server is yugen.us.to", value="Contact admins for any other concerns",
+                     inline=False)
     rembed.set_footer(text="Thank you!")
     await ctx.channel.purge(limit=1)
     await ctx.channel.send(embed=rembed)
+    time.sleep(0.5)
+    await ctx.channel.send(
+        "**Also check the bots help page for all the commands!** https://github.com/leifadev/survival-guy/wiki")
+
 
 @bot.command()
 @commands.has_role("Admin")
 async def hypixelembed(ctx):
     hembed = discord.Embed(title="React here to see your Hypixel stats", color=0x77bb41)
-    hembed.add_field(name="Click the checkmark and have fun!", value="Brought to you by the Hypixel Guild Bot", inline=False)
+    hembed.add_field(name="Click the checkmark and have fun!", value="Brought to you by the Hypixel Guild Bot",
+                     inline=False)
     await ctx.channel.purge(limit=1)
     await ctx.channel.send(embed=hembed)
 
+
+@bot.command()
+async def sourcecode(ctx):
+    await ctx.channel.purge(limit=1)
+    await ctx.channel.send("**The code for the bot is public to everyone on github**:")
+    await ctx.channel.send("https://github.com/leifadev/survival-guy/")
+
+
+## VOICE STUFF
+
+@bot.command()
+@commands.cooldown(1, 20, commands.BucketType.user)
+async def calmdown(ctx):
+    try:
+        channel = ctx.message.author.voice.channel
+        await channel.connect()
+        voice = get(bot.voice_clients)
+        voice.play(discord.FFmpegPCMAudio('sounds/calmdownjamal.wav'))
+        voice.source = discord.PCMVolumeTransformer(voice.source)
+        voice.source.volume = 20.0
+        time.sleep(4.5)
+        await voice.disconnect()
+    except:
+        await ctx.author.send("You have to be in a voice channel to use this!")
+
+@bot.command()
+@commands.cooldown(1, 20, commands.BucketType.user)
+async def pierre(ctx):
+    try:
+        channel = ctx.message.author.voice.channel
+        await channel.connect()
+        voice = get(bot.voice_clients)
+        voice.play(discord.FFmpegPCMAudio('sounds/pierre.wav'))
+        voice.source = discord.PCMVolumeTransformer(voice.source)
+        voice.source.volume = 10.0
+        time.sleep(4.5)
+        await voice.disconnect()
+    except:
+        await ctx.author.send("You have to be in a voice channel to use this!")
+        await ctx.channel.purge(limit=1)
+        
+@bot.command()
+@commands.has_role("Admin")
+async def eclear(ctx, amount):
+    try:
+    	await ctx.channel.purge(limit=int(amount))
+    except:
+        await ctx.channel.send("Please specify a valid number!")
+        
+@bot.command()
+async def hawaii(ctx):
+    await ctx.channel.purge(limit=1)
+    await ctx.channel.send(file=discord.File('pics/hawaii.jpeg'))
+    
 print("Startup successful!")
 
 bot.run(token)
